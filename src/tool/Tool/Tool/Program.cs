29f371f753b7069655e646fc9b96732cb292ffc6
hushuqing <850271634@qq.com>
2019-11-30 18:03:08 +0800

    woxiugia

--- src/tool/Tool/Tool/Program.cs
+++ src/tool/Tool/Tool/Program.cs
@@ -17,6 +17,7 @@ namespace Tool
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            var aa = "";
         }
     }
 }
